f82a1753affac31d74118bf291bffc855d93d1bf
Boris Bliznioukov <boris.blizniukov@uni.lu>
2025-02-14 13:30:17 +0300

    update chatflow ID in App component
    Signed-off-by: Boris Bliznioukov <boris.blizniukov@uni.lu>

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,13 @@ import type { Component } from 'solid-js';
 
 
 const App: Component = () => {
-  return (
-    <>
-      <div class='bg-gradient-to-br from-slate-300 to-slate-100 h-screen w-full'>
-        <Bubble chatflowid="72f46335-8170-4a7e-9405-3b3cc629153c" apiHost="https://ai.blibb.ru" />
-      </div>
-    </>
-  )
+    return (
+        <>
+            <div class='bg-gradient-to-br from-slate-300 to-slate-100 h-screen w-full'>
+                <Bubble chatflowid="0f90a5c2-01a2-47b0-b6a9-509c61927011" apiHost="https://ai.blibb.ru" />
+            </div>
+        </>
+    )
 }
 
 export default App
